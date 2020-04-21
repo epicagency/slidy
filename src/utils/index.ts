222e391cde22247153ee724286e36b4e3fa62804
Thierry Michel <thmichel@gmail.com>
2020-04-21 17:56:11 +0200

    improvement(root): :fire: remove obsolete check (FF25)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -22,14 +22,8 @@ export function parents(element: HTMLElement, className: string) {
  * Check for touchevents.
  */
 
-class DocumentTouch {}
-
 export function touchevents() {
-  if (
-    'ontouchstart' in window ||
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    ((window as any).DocumentTouch && document instanceof DocumentTouch)
-  ) {
+  if ('ontouchstart' in window) {
     return true
   }
 

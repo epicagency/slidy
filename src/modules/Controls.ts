5a46ab73aac9ee02612fddb8741d347cba52e7a3
Thierry Michel <thmichel@gmail.com>
2020-09-18 11:48:36 +0200

    fix: :fire: remove wrong el

--- src/modules/Controls.ts
+++ src/modules/Controls.ts
@@ -38,7 +38,6 @@ export class Controls {
    * Destroy component.
    */
   public destroy(): void {
-    this._el = document.querySelector('.slidy-controls')
     this._el.parentNode.removeChild(this._el)
     this._slidy.hooks.remove('beforeSlide', this._update)
   }
